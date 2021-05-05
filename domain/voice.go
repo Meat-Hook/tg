38c48206692646dcc0c88cd74c753c9205e5df88
Edgar <edo7796@gmail.com>
2021-05-05 10:23:59 +0300

    chore: fix vouce

---
+++ domain/voice.go
@@ -0,0 +1,17 @@
+package domain
+
+// Voice this object represents a voice note.
+type Voice struct {
+	// FileID identifier for this file, which can be used to download or reuse the file.
+	FileID string `json:"file_id"`
+	// FileUniqueID unique identifier for this file,
+	// which is supposed to be the same over time and for different bots.
+	// Can't be used to download or reuse the file.
+	FileUniqueID string `json:"file_unique_id"`
+	// Duration of the audio in seconds as defined by sender.
+	Duration int `json:"duration"`
+	// MimeType MIME type of the file as defined by sender.
+	MimeType string `json:"mime_type"`
+	// FileSize file size.
+	FileSize int `json:"file_size"`
+}
